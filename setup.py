9737eef50ceefdf7285fcd6adc872f6276d0070c
James Wong <2421248+jameswnl@users.noreply.github.com>
2020-03-16 13:42:35 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name="tatd",
+    version="0.1",
+    description="Tower Analytics Test Data",
+    author="James Wong",
+    author_email="jwong@redhat.com",
+    url="https://github.com/RedHatInsights/tower-analytics-test-data",
+    packages=["tatd"],
+)
